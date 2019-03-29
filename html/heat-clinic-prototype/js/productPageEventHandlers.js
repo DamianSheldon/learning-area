b2ded4ea57bd52f16630f01a9b1911036631edfb
Meiliang Dong <dongmeilianghy@sina.com>
2019-03-29 15:51:51 +0800

    Improve tab nav control

--- html/heat-clinic-prototype/js/productPageEventHandlers.js
+++ html/heat-clinic-prototype/js/productPageEventHandlers.js
@@ -3,6 +3,4 @@
     $('.product-card img').each(function(i, el) {
         $(el).parent().zoom({magnify:2, on: 'grab'});
     });
-
-    // $('#main-product-img').zoom({magnify:2, on: 'grab'});
 })(jQuery);
